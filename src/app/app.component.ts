0d5f6fb863a5ecbdde9accce046457b643d5fb84
nsaid@youxel.com <nsaid@youxel.com>
2019-07-30 22:37:44 +0200

    reaload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import { TranslateService } from '@ngx-translate/core';
 import {UserService} from "./utils/services/user.service";
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'ngx-app',
@@ -16,10 +17,21 @@ export class AppComponent implements OnInit {
 
   constructor(private analytics: AnalyticsService,
               private translate: TranslateService,
-              private userService: UserService) {
+              private userService: UserService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
+    this.router.routeReuseStrategy.shouldReuseRoute = function(){
+    return false;
+    };
+
+    this.router.events.subscribe((evt) => {
+        if (evt instanceof NavigationEnd ) {
+            this.router.navigated = false;
+            window.scrollTo(0, 0);
+        }
+    });
     this.analytics.trackPageViews();
     const user = sessionStorage.getItem('user');
     if (user !== null) {
